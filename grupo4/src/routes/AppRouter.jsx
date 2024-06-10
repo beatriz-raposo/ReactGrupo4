2e8f2cb17b0e44cf1158e6e5c22a0d62b9f4f703
iza.pdelucca <bellapdelucca@gmail.com>
2024-06-10 18:12:35 -0300

    feat: adicionando rotas

---
+++ grupo4/src/routes/AppRouter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Login from '../pages/Login';
+import Feed from '../pages/Feed';
+import Posts from '../pages/Post';
+import Register from '../pages/Register';
+import Update from '../pages/Update';
+
+function AppRouter() {
+  return (
+    <Routes>
+        <Route path="/" element={<Login/>}></Route>
+        <Route path="/feed" element={<Feed/>}></Route>
+        <Route path="/post" element={<Posts/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
+        <Route path="/update" element={<Update/>}></Route>
+    </Routes>
+  );
+}
+
+export default AppRouter;
