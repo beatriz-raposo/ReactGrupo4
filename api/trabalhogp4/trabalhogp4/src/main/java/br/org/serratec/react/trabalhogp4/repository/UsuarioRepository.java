7b7587cebbc2949fad10d689571a676bdad8ec72
Rafaela Beber <rafaelabebercoelho@gmail.com>
2024-06-10 17:12:33 -0300

    fix: usuario repository

--- api/trabalhogp4/trabalhogp4/src/main/java/br/org/serratec/react/trabalhogp4/repository/UsuarioRepository.java
+++ api/trabalhogp4/trabalhogp4/src/main/java/br/org/serratec/react/trabalhogp4/repository/UsuarioRepository.java
@@ -1,5 +1,10 @@
 package br.org.serratec.react.trabalhogp4.repository;
 
-public class UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.org.serratec.react.trabalhogp4.model.Usuario;
+		
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
 }
