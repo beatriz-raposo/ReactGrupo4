82e3e56e324663eb38369c01abc71c4b82b67861
Felipe Cauê <felipecaue7@hotmail.com>
2024-06-10 16:44:47 -0300

    api
    feat: subindo api para construção

---
+++ api/trabalhogp4/trabalhogp4/src/main/java/br/org/serratec/react/trabalhogp4/Trabalhogp4Application.java
@@ -0,0 +1,13 @@
+package br.org.serratec.react.trabalhogp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalhogp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalhogp4Application.class, args);
+	}
+
+}
