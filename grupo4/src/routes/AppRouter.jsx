70f42721dff151d0388ec245f1b070bbc89084ec
beatriz-raposo <162373882+beatriz-raposo@users.noreply.github.com>
2024-06-11 18:24:58 -0300

    Fix: deletando pasta que não ira usar.

--- grupo4/src/routes/AppRouter.jsx
+++ grupo4/src/routes/AppRouter.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Login from '../pages/Login';
 import Feed from '../pages/Feed';
 import Posts from '../pages/Post';
-import Register from '../pages/Register';
 import Update from '../pages/Update';
 import Sobre from '../pages/Sobre';
 
@@ -13,7 +12,6 @@ function AppRouter() {
         <Route path="/" element={<Login/>}></Route>
         <Route path="/feed" element={<Feed/>}></Route>
         <Route path="/post" element={<Posts/>}></Route>
-        <Route path="/register" element={<Register/>}></Route>
         <Route path="/sobre" element={<Sobre/>}></Route>
         <Route path="/update" element={<Update/>}></Route>
     </Routes>
