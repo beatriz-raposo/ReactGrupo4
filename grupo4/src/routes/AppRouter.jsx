ea3d4cabfcf67a95cbd769b59613d893e5c11f8c
beatriz-raposo <162373882+beatriz-raposo@users.noreply.github.com>
2024-06-10 22:40:59 -0300

    Fix: Ajustando rota para sobre.

--- grupo4/src/routes/AppRouter.jsx
+++ grupo4/src/routes/AppRouter.jsx
@@ -5,6 +5,7 @@ import Feed from '../pages/Feed';
 import Posts from '../pages/Post';
 import Register from '../pages/Register';
 import Update from '../pages/Update';
+import Sobre from '../pages/Sobre';
 
 function AppRouter() {
   return (
@@ -13,6 +14,7 @@ function AppRouter() {
         <Route path="/feed" element={<Feed/>}></Route>
         <Route path="/post" element={<Posts/>}></Route>
         <Route path="/register" element={<Register/>}></Route>
+        <Route path="/sobre" element={<Sobre/>}></Route>
         <Route path="/update" element={<Update/>}></Route>
     </Routes>
   );
