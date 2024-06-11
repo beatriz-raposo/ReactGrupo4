704ea352766c62ac66f6fc1397e6c044a333870e
beatriz-raposo <162373882+beatriz-raposo@users.noreply.github.com>
2024-06-11 18:28:49 -0300

    fix: Ajustando import.

--- grupo4/src/routes/AppRouter.jsx
+++ grupo4/src/routes/AppRouter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
-import Login from '../pages/Home';
+import Home from '../pages/Home';
 import Feed from '../pages/Feed';
 import Posts from '../pages/Post';
 import Update from '../pages/Update';
